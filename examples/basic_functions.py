c781a723c99853fed0a04854faac464ff83c7447
Renan Santana <renan_ota@hotmail.com>
2021-02-09 19:34:08 -0300

    Funções básicas

---
+++ examples/basic_functions.py
@@ -0,0 +1,28 @@
+# https://github.com/murtazahassan/OpenCV-Python-Tutorials-and-Projects
+import cv2
+import numpy as np
+
+
+path = "..\\images\\card.jpg"
+image = cv2.imread(path)
+
+# Grayscale
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Blur
+image_blur = cv2.GaussianBlur(image, (7, 7), 0)
+
+# Canny (imagem toda preta com um contorno branco nos detalhes)
+image_canny = cv2.Canny(image, 100, 100)
+
+# Dillation
+kernel = np.ones((5, 5), np.uint8)  # Gera uma matriz de 5x5
+print(kernel)
+image_dillation = cv2.dilate(image, kernel, iterations=1)
+
+# Erosion
+image_erosion = cv2.erode(image, kernel, iterations=1)
+
+# Show image
+cv2.imshow("Image", image_erosion)
+cv2.waitKey(0)
