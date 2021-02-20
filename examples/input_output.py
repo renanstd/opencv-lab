83b9dfb57a2b8ae8d281c7ee21597540074cafb2
Renan Santana <renan_ota@hotmail.com>
2021-02-19 23:46:58 -0300

    Corrige nome do preview

--- examples/input_output.py
+++ examples/input_output.py
@@ -12,7 +12,7 @@ image = cv2.imread(IMAGES_DIR + "card.jpg", cv2.IMREAD_GRAYSCALE)
 # cv2.imwrite("name.png", image)
 
 # Exibir imagem
-cv2.imshow("Press Q to close", image)
+cv2.imshow("Preview", image)
 
 # Manter a imagem aberta até que o usuário tome alguma ação
 cv2.waitKey(0)
